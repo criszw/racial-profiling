6a1df0c3313539fe91104862c87643e384433681
criszw <czaldumbidew@hotmail.com>
2019-04-22 11:47:55 -0500

    Update odds-ratio.R

--- odds-ratio.R
+++ odds-ratio.R
@@ -12,19 +12,17 @@ summary(model)
 ```
 
 #Estimating odds ratio
-```{r}
 exp(model$coefficients[2])
-```
-
-It is 79% less likely that a white person is stopped by TSA compared to a non white person. 
 
 
-# Is there an association between skin color (white vs non-white) and being stopped by TSA?
+#It is 79% less likely that a white person is stopped by TSA compared to a non white person. 
 
-```{r}
+
+#Is there an association between skin color (white vs non-white) and being stopped by TSA?
+
 model.0<-glm(cbind(stopped,not.stopped)~1,family=binomial)
 anova(model.0,model,test="Chisq")
-```
 
-Since p-value is 0.01705 there is considerable evidence at 5% significance level that there is an association between skin color and being stopped by TSA. 
+#Since p-value is 0.01705 there is considerable evidence at 5% significance level that there 
+#is an association between skin color and being stopped by TSA. 
 
